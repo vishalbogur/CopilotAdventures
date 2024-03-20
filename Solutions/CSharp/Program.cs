be151e3a8db13fd0acda695bc13f10c9ad815a03
Jordy Noom <jonoom@schubergphilis.com>
2024-03-20 14:59:22 +0100

    trimming

--- Solutions/CSharp/Program.cs
+++ Solutions/CSharp/Program.cs
@@ -2,40 +2,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        var adventure = args?.FirstOrDefault()?.ToLower();
-        if (string.IsNullOrWhiteSpace(adventure))
-        {
-            Console.WriteLine("Please specify which logic to run: Sample names include: algora, chamberofechoes, elodoria, lumoria, mythos, stonevale, tempora.");
-            adventure = Console.ReadLine();
-        }
-
-        switch (adventure)
-        {
-            case "algora":
-                Algora.Run();
-                break;
-            case "chamberofechoes":
-                ChamberOfEchoes.Run();
-                break;
-            case "eldoria":
-                Eldoria.Run();
-                break;
-            case "lumoria":
-                Lumoria.Run();
-                break;
-            case "mythos":
-                Mythos.Run();
-                break;
-            case "stonevale":
-                StoneVale.Run();
-                break;
-            case "tempora":
-                Tempora.Run();
-                break;
-            default:
-                Console.WriteLine($"Unknown option: {adventure}");
-                break;
-        }
+        Algora.Run();
     }
 }
